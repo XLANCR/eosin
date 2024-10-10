38996a4ea6f8834316ad701e0218230b6c32d186
sachin-sankar <mail.sachinsankar@gmail.com>
2024-10-10 23:16:09 +0530

    fix: Fixed import error and removed debug statement

--- eosin/__main__.py
+++ eosin/__main__.py
@@ -1,16 +1,10 @@
 import argparse
-from parser import Parser as PDFParser
+from eosin.parser import Parser as PDFParser
 
 if __name__ == "__main__":
-    try:
-        args_parser = argparse.ArgumentParser(description="Bank Statement Parser CLI")
-        args_parser.add_argument("file", help="File to parse")
-        args = args_parser.parse_args()
+    args_parser = argparse.ArgumentParser(description="Bank Statement Parser CLI")
+    args_parser.add_argument("file", help="File to parse")
+    args = args_parser.parse_args()
 
-        parser = PDFParser(args.file)
-        print(parser.parse())
-
-    # TODO: Remove this when done testing
-    except:
-        parser = PDFParser("test.pdf")
-        print(parser.parse())
+    parser = PDFParser(args.file)
+    print(parser.parse())
